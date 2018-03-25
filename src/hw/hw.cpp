320df74076a9140580330d014bb594d0a792c945
Mark Prediger <markprediger@gmail.com>
2018-03-25 17:07:34 +0200

    Increase adc speed, 4x sampling
    according to http://nicecircuits.com/playing-with-analog-to-digital-
    converter-on-arduino-due/

--- src/hw/hw.cpp
+++ src/hw/hw.cpp
@@ -15,6 +15,19 @@ namespace hw {
 void init() {
 	analogReadResolution(12);
 
+	pmc_enable_periph_clk(ID_ADC); // To use peripheral, we must enable clock distributon to it
+    adc_init(ADC, SystemCoreClock, ADC_FREQ_MAX, ADC_STARTUP_FAST); // initialize, set maximum posibble speed
+    adc_disable_interrupt(ADC, 0xFFFFFFFF);
+    adc_set_resolution(ADC, ADC_12_BITS);
+    adc_configure_power_save(ADC, 0, 0); // Disable sleep
+    adc_configure_timing(ADC, 0, ADC_SETTLING_TIME_3, 1); // Set timings - standard values
+    adc_set_bias_current(ADC, 1); // Bias current - maximum performance over current consumption
+    adc_stop_sequencer(ADC); // not using it
+    adc_disable_tag(ADC); // it has to do with sequencer, not using it
+    adc_disable_ts(ADC); // deisable temperature sensor
+    adc_configure_trigger(ADC, ADC_TRIG_SW, 1); // triggering from software, freerunning mode
+    adc_disable_all_channel(ADC);
+
 	static Adafruit_MotorShield AFMS_L0(0x60);
 	static Adafruit_MotorShield AFMS_L1(0x61);
 
@@ -42,7 +55,7 @@ void init() {
 }
 
 float adc::getChannel(size_t channel) {
-	return analogRead(channel);
+	return (analogRead(channel) + analogRead(channel) + analogRead(channel) + analogRead(channel)) / 4.0f;
 }
 
 uint32_t clock::getMsTick() {
